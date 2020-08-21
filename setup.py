55222c065e66cc9a03d9f929c7da562173e740e9
Simone Bacchio <s.bacchio@gmail.com>
2020-08-21 12:35:49 +0300

    Updating setup files

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
 import sys
 
-try:
-    from lyncs_setuptools import setup, CMakeExtension
-    from lyncs_clime import __path__ as lime_path
-
-    # from lyncs_DDalphaAMG import __path__ as DDalphaAMG_path
-except ImportError:
-    print(
-        """
-    
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    Install first the requirements:
-    pip install -r requirements.txt
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    
-    """
-    )
-    sys.exit(0)
+from lyncs_setuptools import setup, CMakeExtension
+from lyncs_clime import __path__ as lime_path
 
 setup(
     "lyncs_tmLQCD",
@@ -25,7 +10,7 @@ setup(
         CMakeExtension("lyncs_tmLQCD.lib", ".", ["-DLIME_PATH=%s" % lime_path[0],])
     ],
     data_files=[(".", ["config.py.in"])],
-    install_requires=["lyncs-cppyy", "lyncs-clime",],  # "lyncs-mpi","lyncs-DDalphaAMG"
+    install_requires=["lyncs-cppyy", "lyncs-clime", "numpy",],
     keywords=[
         "Lyncs",
         "tmLQCD",
